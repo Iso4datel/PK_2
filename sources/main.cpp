e4f7ab2f8d7379127878c640a9e58435f47090d0
Iso4datel <ZYXWorking@mail.ru>
2018-04-13 05:36:15 -0700

    update

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,9 +2,10 @@
 #include <cstdlib>
 #include <string>
 
-int main() {
-
-	std::string name = std::getenv("SOMETHING_VAR");
+int main(int argc, char* argv[]) {
+	
+	std::cout << "Вы ввели: " << *(argv + 1) << std::endl;
+	std::string name = std::getenv(*(argv + 1));
 	std::cout << name << std::endl;
 
 	return 0;
